689e011e0fdb718299f955565e8be2e6272f42bf
Your Name <your_email@example.com>
2026-07-28 11:07:57 -0300

    chore: cria pagina componente tabela-lotes

---
+++ src/app/features/pages/consulta-lotes/consulta-lotes.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ConsultaLotesComponent } from './consulta-lotes.component';
+
+describe('ConsultaLotesComponent', () => {
+  let component: ConsultaLotesComponent;
+  let fixture: ComponentFixture<ConsultaLotesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ConsultaLotesComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ConsultaLotesComponent);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/features/pages/consulta-lotes/consulta-lotes.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consulta-lotes',
+  imports: [],
+  templateUrl: './consulta-lotes.component.html',
+  styleUrl: './consulta-lotes.component.scss',
+})
+export class ConsultaLotesComponent {}
