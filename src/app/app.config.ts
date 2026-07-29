aa7178dc1a7970828a577b0e3f65946c8c1d065b
Your Name <your_email@example.com>
2026-07-29 17:12:46 -0300

    chore: configura rotas e locale pt-BR

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,15 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+import { LOCALE_ID } from '@angular/core';
 import { routes } from './app.routes';
+registerLocaleData(localePt, 'pt-BR');
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+  ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/pages/consulta-lotes/consulta-lotes.component').then(
+        (m) => m.ConsultaLotesComponent,
+      ),
+  },
+];
