14f6820e17c597ca29a08193cbb6a0fb2c887fb2
Your Name <your_email@example.com>
2026-07-29 17:06:14 -0300

    feat: adiciona componente filtro-lotes

--- src/app/features/components/filtro-lotes/filtro-lotes.component.ts
+++ src/app/features/components/filtro-lotes/filtro-lotes.component.ts
@@ -1,9 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, inject, Output } from '@angular/core';
+import { LoteService } from '../../services/lote/lote.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { rangeValidator } from '../../../shared/validators/range.validator';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { FiltroLoteBusca } from '../../../shared/models/filtro-lote-busca.model';
+import { debounceTime, Subject } from 'rxjs';
+import { SituacaoLote } from '../../../shared/enum/situacao-lote.enum';
 
 @Component({
   selector: 'app-filtro-lotes',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './filtro-lotes.component.html',
   styleUrl: './filtro-lotes.component.scss',
 })
-export class FiltroLotesComponent {}
+export class FiltroLotesComponent {
+  @Output() pesquisar = new EventEmitter<FiltroLoteBusca>();
+
+  situacoesLote = Object.values(SituacaoLote);
+  private fb = inject(FormBuilder);
+  private pesquisarClick$ = new Subject<void>();
+
+  constructor() {
+    this.pesquisarClick$.pipe(debounceTime(300)).subscribe(() => {
+      console.log('Teste do debounceTime');
+      this.pesquisar.emit(this.filtroForm.value as FiltroLoteBusca);
+    });
+  }
+
+  filtroForm = this.fb.nonNullable.group(
+    {
+      instituicao: [''],
+      instituicaoResp: [''],
+      situacaoLote: [''],
+      valorLoteDe: [''],
+      valorLoteAte: [''],
+      dataEntradaDe: [''],
+      dataEntradaAte: [''],
+      idLoteDe: [''],
+      idLoteAte: [''],
+    },
+    {
+      validators: [
+        rangeValidator('valorLoteDe', 'valorLoteAte'),
+        rangeValidator('dataEntradaDe', 'dataEntradaAte'),
+        rangeValidator('idLoteDe', 'idLoteAte'),
+      ],
+    },
+  );
+
+  onSubmit(): void {
+    this.pesquisarClick$.next();
+  }
+}
